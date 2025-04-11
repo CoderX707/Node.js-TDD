1ba865b8f3c3918818fa0425f378bdbf9b85e703
Ashish Mule <ashishmule707@gmail.com>
2025-04-11 16:37:27 +0530

    Second test: convert string number to actual number

--- tests/calculator.spec.ts
+++ tests/calculator.spec.ts
@@ -3,3 +3,7 @@ import { add } from "../src/calculator"
 test('Return 0 for empty string',()=>{
     expect(add('')).toBe(0)
 })
+
+test('Convert a number in string format into an actual number.',()=>{
+    expect(add('12')).toBe(12)
+})
